30f4deb10959bc9e4468c50a28ea034ecdb254a8
Thiago Assis <thiagoassis.99@gmail.com>
2020-09-02 17:06:20 -0300

    funcionando com formatação do run.codes

--- 20193008396.c
+++ 20193008396.c
@@ -1,9 +1,7 @@
 /**
-
-  Este algoritmo foi adaptado, usando como base o algoritmo de Dijkstra desenvolvido por RITA PERES, no seguinte site da ufmg:
+  @Author Thiago de Assis Lima
+  Este algoritmo foi adaptado, usando como base o algoritmo de Dijkstra desenvolvido por RITA PERES, no seguinte site:
   https://www.revista-programar.info/artigos/algoritmo-de-dijkstra/
-  Outras referencias:
-  https://stackoverflow.com/questions/4834811/strcat-concat-a-char-onto-a-string
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -17,7 +15,7 @@ int NUMERO_CIDADES = 0;
 int destino, origem, vertices = 0;
 int custo, *custos = NULL;
 
-void dijkstra(int vertices, int origem, int destino, int *custos)
+void dijkstra(int vertices, int origem, int destino, int *custos, char *chave_das_cidades)
 {
 	int i, v, cont = 0;
 	int *ant, *tmp;
@@ -87,7 +85,7 @@ void dijkstra(int vertices, int origem, int destino, int *custos)
 	} while (v != destino - 1 && min != HUGE_VAL);
 
 	/* Mostra o Resultado da procura */
-	printf("\tDe %d para %d: \t", origem, destino);
+	printf("Menor percurso:");
 	if (min == HUGE_VAL)
 	{
 		printf("Nao Existe\n");
@@ -107,86 +105,12 @@ void dijkstra(int vertices, int origem, int destino, int *custos)
 
 		for (i = cont; i > 0; i--)
 		{
-			printf("%d -> ", tmp[i - 1]);
+			printf(" %c", chave_das_cidades[tmp[i - 1] - 1]);
 		}
-		printf("%d", destino);
+		printf(" %c\n", chave_das_cidades[destino - 1]);
 
-		printf("\n\tCusto: %d\n", (int)dist[destino - 1]);
+		printf("Distancia total: %d Km\n", (int)dist[destino - 1]);
 	}
-}
-
-void cabecalho(void)
-{
-	printf("Implementacao do Algoritmo de Dijasktra\n");
-	printf("Comandos:\n");
-	printf("\t d - Adicionar Grafo\n"
-		   "\t r - Procura Os Menores Caminhos no Grafo\n"
-		   "\t CTRL+C para Sair do programa\n");
-	printf("-------- ");
-}
-
-void add(void)
-{
-	int i, j;
-
-	do
-	{
-		printf("\nQual o numero de vertices (numero minimo = 2 ): ");
-		scanf("%d", &vertices);
-	} while (vertices < 2);
-
-	if (!custos)
-		free(custos);
-	custos = (int *)malloc(sizeof(int) * vertices * vertices);
-	for (i = 0; i <= vertices * vertices; i++)
-		custos[i] = -1;
-
-	printf("Insira as arestas:\n");
-	do
-	{
-		do
-		{
-			printf("Origem da aresta (entre 1 e %d ou '0' para sair): ", vertices);
-			scanf("%d", &origem);
-		} while (origem < 0 || origem > vertices);
-
-		if (origem)
-		{
-			do
-			{
-				printf("Destino da aresta (entre 1 e %d, menos %d): ", vertices, origem);
-				scanf("%d", &destino);
-			} while (destino < 1 || destino > vertices || destino == origem);
-
-			do
-			{
-				printf("Custo (positivo) da aresta do vertice %d para o vertice %d: ",
-					   origem, destino);
-				scanf("%d", &custo);
-			} while (custo < 0);
-
-			custos[(origem - 1) * vertices + destino - 1] = custo;
-		}
-	} while (origem);
-}
-
-void procurar(void)
-{
-	int i, j;
-
-	/* Azul */
-	printf("Lista dos Menores Caminhos no Grafo Dado: \n");
-
-	for (i = 1; i <= vertices; i++)
-	{
-		for (j = 1; j <= vertices; j++)
-			dijkstra(vertices, i, j, custos);
-		printf("\n");
-	}
-
-	printf("ENTER para voltar ao menu inicial>\n");
-	/* Volta cor normal */
-	printf("\033[m");
 }
 
 int verificaSeCidadeJaExiste(char *cidades, char nome_cidade)
@@ -214,31 +138,28 @@ int retornaChaveCidadePeloNome(char *cidades, char nome_cidade)
 int main(int argc, char **argv)
 {
 	FILE *arquivo;
-	FILE *arquivo2;
 	char nome_arquivo[100];
 
 	char cidadeX, cidadeY;
 	int valor_distancia = 0;
-	char char_arquivo = '\0';
 	int count_arestas = 0;
-	int key_cidade = 0;
 	int numero_arestas = 0;
 	char *cidades;
 	char *chave_das_cidades;
 	int *custos_from_file;
 	int k = 0;
 	int j = 0;
-	printf("Digite o nome do arquivo de entrada: \n");
-	// scanf("%s", nome_arquivo);
-	arquivo = fopen("teste.txt", "r+");
-	char_arquivo = fscanf(arquivo, "%d\n", &numero_arestas);
+	printf("Digite o nome do arquivo de entrada: ");
+	scanf("%s", nome_arquivo);
+	arquivo = fopen(nome_arquivo, "r+");
+	fscanf(arquivo, "%d\n", &numero_arestas);
 
 	cidades = malloc(numero_arestas * 2 * sizeof(char));
 	chave_das_cidades = malloc(numero_arestas * sizeof(char));
 	custos_from_file = malloc(numero_arestas * sizeof(int));
 	do
 	{
-		char_arquivo = fscanf(arquivo, "%c %c %d\n", &cidadeX, &cidadeY, &valor_distancia);
+		fscanf(arquivo, "%c %c %d\n", &cidadeX, &cidadeY, &valor_distancia);
 		cidades[count_arestas] = cidadeX;
 		cidades[count_arestas + 1] = cidadeY;
 		custos_from_file[j] = valor_distancia;
@@ -261,9 +182,8 @@ int main(int argc, char **argv)
 		count_arestas += 2;
 	} while (count_arestas < numero_arestas * 2);
 
-	char_arquivo = fscanf(arquivo, "%c %c\n", &cidadeX, &cidadeY);
+	fscanf(arquivo, "%c %c\n", &cidadeX, &cidadeY);
 
-	printf("saiu do while\n");
 	fclose(arquivo);
 
 	int i;
@@ -281,20 +201,16 @@ int main(int argc, char **argv)
 	{
 		origem = retornaChaveCidadePeloNome(chave_das_cidades, cidades[i]) + 1;
 		destino = retornaChaveCidadePeloNome(chave_das_cidades, cidades[i + 1]) + 1;
-		int aux = (origem - 1) * NUMERO_CIDADES + destino - 1;
 
 		custos[(origem - 1) * NUMERO_CIDADES + destino - 1] = custos_from_file[j];
 		custos[(destino - 1) * NUMERO_CIDADES + origem - 1] = custos_from_file[j];
 		j++;
 	}
 
-	i = 0;
-	j = 0;
-	dijkstra(NUMERO_CIDADES, retornaChaveCidadePeloNome(chave_das_cidades, cidadeX) + 1, retornaChaveCidadePeloNome(chave_das_cidades, cidadeY) + 1, custos);
+	dijkstra(NUMERO_CIDADES, retornaChaveCidadePeloNome(chave_das_cidades, cidadeX) + 1, retornaChaveCidadePeloNome(chave_das_cidades, cidadeY) + 1, custos, chave_das_cidades);
 
 	// free(chave_das_cidades);
 	// free(custos_from_file);
-	printf("saiu do while? %d", NUMERO_CIDADES);
 
 	return 0;
 }
